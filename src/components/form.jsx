fdd89feb179290e788a591a8811ab65987e3d3c0
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-19 14:08:40 -0300

    Criado useState text para armazenar o valor do texto inserido

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 //importando do material
 import { TextField, Button, Paper } from "@mui/material"; 
 
 export default function Form(){
+    
+    const [text, setText] = useState(null); // Variável para guardar o texto digitado
+
+
     return(
         <Paper style={{ padding: "1em" }}>
             <div style={{ display: "flex", justifyContent: "center" }}>
-                <TextField id="outlined-basic" label="Tarefa" variant="outlined"  fullWidth/>
+                <TextField 
+                id="outlined-basic" 
+                label="Tarefa" 
+                variant="outlined" 
+                onChange={(e) => setText(e.target.value)} 
+                fullWidth/>
                 <Button variant="text">ADD</Button>
             </div>
         </Paper>
