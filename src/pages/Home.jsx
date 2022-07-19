4c6f053272d05edeebbee8510b3ab78ef1f354fe
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-19 16:46:44 -0300

    Atualizado para poder editar os itens da lista de tarefas ao clicar  abrir o dialog

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,13 +28,29 @@ export default function Home(){
         setTodos(filtered);
     }
 
+    const editTodo = (id, editedText) => {
+        todos.splice(id, 1, {text:editedText, id: id})
+
+        let todosArray = [...todos]; // Cópia do array todos
+
+        // for para não criar outro item na lista quando editamos
+
+        for(let i in todosArray){
+            if(todosArray[i].id === id){
+                todosArray[i].text = editedText;
+            }
+        }
+
+        setTodos(todosArray);
+    }
+
     return(
         <Container maxWidth="xs" style={{marginTop: "1em"}}> 
             <Form addTodo={addTodo}/>
             <List sx={{ marginTop: '1em' }}>
                 {todos.map((todo) => 
                 <div style={{ marginTop: '1em' }} key={todo.id}>
-                    <ToDoItem todo={todo} deleteTodo={deleteTodo}/>
+                    <ToDoItem todo={todo} deleteTodo={deleteTodo} editTodo={editTodo}/>
                 </div>                
                 )}  
             </List>
