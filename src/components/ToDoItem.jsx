b288a3b64b9f9f6a10e772b61b237c534590bb7b
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-19 12:28:13 -0300

    Atualizado ícone da lista e inserido texto no primay para aparecer na tela

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -7,6 +7,7 @@ import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import CommentIcon from '@mui/icons-material/Comment';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function ToDoItem() {
 
@@ -14,7 +15,7 @@ export default function ToDoItem() {
     <ListItem
     secondaryAction={
       <IconButton edge="end" aria-label="comments">
-        <CommentIcon />
+        <DeleteIcon />
       </IconButton>
     }
     disablePadding
@@ -27,7 +28,7 @@ export default function ToDoItem() {
           disableRipple
         />
       </ListItemIcon>
-      <ListItemText/>
+      <ListItemText primary={'Line Item'}/>
     </ListItemButton>
   </ListItem>
   );
