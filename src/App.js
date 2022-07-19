b7cee75768e7096f6d11f7e51601d403a4d98554
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-19 11:59:14 -0300

    Importado List do material

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import { Container } from '@mui/system';
+
+import { Container, List } from '@mui/material/';
+
 import './App.css';
 
 import Form from './components/Form';
 import ToDoItem from './components/ToDoItem';
 
+
 function App() {
   return (
     <Container maxWidth="xs" style={{marginTop: "1em"}}> 
       <Form />
+      <List sx={{ width: '100%', maxWidth: '100%', bgcolor: 'background.paper' }}>
       <ToDoItem />
+
+      </List>
     </Container>
   );
 }
